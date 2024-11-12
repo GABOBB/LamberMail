061513258a43303522050de9d73b17574791625d
Gabriel BB <gabbol2002@gmail.com>
2024-11-12 08:24:25 -0600

    se altera el servidor para poder tener mas de un cliente conectado a la vez

--- Server/main.cpp
+++ Server/main.cpp
@@ -1,7 +1,6 @@
 //
-// Created by yerik on 11/11/24.
+// Servidor con soporte para múltiples clientes usando select()
 //
-
 
 #include <iostream>
 #include <sys/socket.h>
@@ -10,16 +9,14 @@
 #include <string>
 #include <unistd.h>
 #include <arpa/inet.h>
+#include <vector>
 
 using namespace std;
 
-int main(int argc, char *argv[]){
-    //std::cout.setf(std::ios::unitbuf);
-    //std::cerr.setf(std::ios::unitbuf);
-
+int main(int argc, char *argv[]) {
     int listening = socket(AF_INET, SOCK_STREAM, 0);
     if (listening == -1) {
-        cerr<<"socket fail"<<endl;
+        cerr << "Socket fail" << endl;
         return -1;
     }
 
@@ -28,65 +25,95 @@ int main(int argc, char *argv[]){
     address.sin_port = htons(54000);
     inet_pton(AF_INET, "127.0.0.1", &address.sin_addr);
 
-    if(bind(listening, (struct sockaddr*)&address, sizeof(address)) == -1) {
-        cerr<<"bind fail"<<endl;
+    if (bind(listening, (struct sockaddr*)&address, sizeof(address)) == -1) {
+        cerr << "Bind fail" << endl;
         return -2;
     }
 
-    if(listen(listening, SOMAXCONN) == -1) {
-        cerr<<"listen fail"<<endl;
+    if (listen(listening, SOMAXCONN) == -1) {
+        cerr << "Listen fail" << endl;
         return -3;
     }
 
-    //acepta la llamada
-    sockaddr_in client;
-    socklen_t C_len = sizeof(client);
-    char host[NI_MAXHOST];
-    char svc[NI_MAXSERV];
+    cout << "Servidor escuchando en 127.0.0.1:54000" << endl;
 
-    int socket_cliente = accept(listening, (struct sockaddr*)&client, &C_len);
+    // Conjuntos de descriptores de archivo para select()
+    fd_set master_set;
+    FD_ZERO(&master_set);
+    FD_SET(listening, &master_set);
+    int max_fd = listening;
 
-    if(socket_cliente == -1) {
-        cerr<<"accept fail"<<endl;
-        return -4;
+    while (true) {
+        fd_set copy_set = master_set;
+
+        // Usar select para monitorear sockets
+        int socket_count = select(max_fd + 1, &copy_set, nullptr, nullptr, nullptr);
+        if (socket_count == -1) {
+            cerr << "Select fail" << endl;
+            break;
+        }
+
+        // Revisar cada socket en el conjunto
+        for (int i = 0; i <= max_fd; i++) {
+            if (FD_ISSET(i, &copy_set)) {
+                // Nueva conexión entrante
+                if (i == listening) {
+                    sockaddr_in client;
+                    socklen_t clientSize = sizeof(client);
+                    int client_socket = accept(listening, (struct sockaddr*)&client, &clientSize);
+
+                    if (client_socket == -1) {
+                        cerr << "Accept fail" << endl;
+                        continue;
+                    }
+
+                    // Añadir nuevo cliente al conjunto maestro
+                    FD_SET(client_socket, &master_set);
+                    if (client_socket > max_fd) {
+                        max_fd = client_socket;
+                    }
+
+                    char host[NI_MAXHOST];
+                    char svc[NI_MAXSERV];
+                    memset(host, 0, NI_MAXHOST);
+                    memset(svc, 0, NI_MAXSERV);
+
+                    int result = getnameinfo((struct sockaddr*)&client, sizeof(client), host, NI_MAXHOST, svc, NI_MAXSERV, 0);
+
+                    if (result) {
+                        cout << host << " conectado en el puerto: " << svc << endl;
+                    } else {
+                        inet_ntop(AF_INET, &client.sin_addr, host, NI_MAXHOST);
+                        cout << host << " conectado en el puerto: " << ntohs(client.sin_port) << endl;
+                    }
+                } else {
+                    // Datos recibidos de un cliente existente
+                    char buf[4096];
+                    memset(buf, 0, 4096);
+                    int bytesR = recv(i, buf, 4096, 0);
+
+                    if (bytesR <= 0) {
+                        // Error o cliente desconectado
+                        if (bytesR == 0) {
+                            cout << "El cliente se desconectó" << endl;
+                        } else {
+                            cerr << "Recv fail" << endl;
+                        }
+                        close(i);
+                        FD_CLR(i, &master_set);
+                    } else {
+                        // Imprimir datos recibidos
+                        std::string data(buf, bytesR);
+                        cout << "Recibido " << bytesR << " bytes: " << i << endl;
+
+                        // Enviar datos de vuelta al cliente
+                        send(i, buf, bytesR, 0);
+                    }
+                }
+            }
+        }
     }
 
     close(listening);
-
-    memset(host, 0, NI_MAXHOST);
-    memset(svc, 0, NI_MAXSERV);
-
-    int result = getnameinfo((struct sockaddr*)&client, sizeof(client),host, NI_MAXHOST, svc, NI_MAXSERV, 0);
-
-    if(result) {
-        cout<<host << " conectado con: "<<svc<<endl;
-    }else {
-        inet_ntop(AF_INET, &client.sin_addr, host, NI_MAXHOST);
-        cout<<host<<" conectado con: "<<ntohs(client.sin_port)<<endl;
-    }
-
-    char buf[4096];
-    while(true) {
-
-        //memset(buf, 0, 4096);
-
-        int bytesR= recv(socket_cliente, buf, 4096, 0);
-
-
-        if(bytesR == -1) {
-            cerr<<"recv fail"<<endl;
-            break;
-        }
-        if(bytesR == 0) {
-            cout<<"el cliente se desconecto"<<endl;
-            break;
-        }
-        std::string string(buf,bytesR);
-        cout<< "recived "<<bytesR<<" bytes: " <<endl;
-
-        send(socket_cliente, buf, bytesR, 0);
-    }
-
-    close(socket_cliente);
     return 0;
 }
