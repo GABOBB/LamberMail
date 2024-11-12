0cbddeddb9afbdf8b253940333f93cb10448287d
Yerik1 <yerikchaves818@gmail.com>
2024-11-12 14:08:16 -0600

    correccion de ubicacion en la encriptacion

--- Client/main.cpp
+++ Client/main.cpp
@@ -4,6 +4,7 @@
 
 //#include "LogIn.h"
 #include "register.h"
+#include "Encription.h"
 #include <QApplication>
 //#include "mainwindow.h"
 
@@ -12,5 +13,10 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     Register w;
     w.show();
+    Encription enc;
+    // Mensaje de prueba largo
+    std::string longMessage = "Mensaje de Prueba para encriptacion";
+    std::pair<std::vector<unsigned char>, std::vector<int>> result =enc.encrypt(longMessage);
+    enc.decrypt(result.first, result.second);
     return a.exec();
 }
